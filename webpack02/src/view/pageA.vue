9748e4c66143f4e68916e90093f4785215cc7307
huchuan <406115565@qq.com>
2018-05-02 17:51:39 +0800

    ''

--- webpack02/src/view/pageA.vue
+++ webpack02/src/view/pageA.vue
@@ -2,11 +2,27 @@
     <div class="b">
         <h1>pageA页面</h1>
         <img src="../assets/img/wx.png">
+        <Param :user='user'></Param>
     </div>
 </template>
 <script>
+    import Param from './param.vue'
     export default {
-        name: 'pageA'
+        name: 'pageA',
+        data() {
+            return { user: '123' }
+        },
+        components: {
+            Param
+        },
+        created(){
+            this.$on('haha', (a) => {
+                alert(a)
+            })
+        },
+        methods: {
+            
+        }
     }
 </script>
 <style>
---
+++ webpack02/src/view/param.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>par12312312312am</h1>
+        <button @click='test'>click</button>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            user: {
+                type: String,
+                default: 'haha'
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {
+
+            test() {
+                this.$parent.$emit('haha', 'haha')
+            }
+        }
+    }
+</script>
